8834af6f25c66bd521516a50da73c723ff50750d
김민준 <ming@Minjuns-MacBook-Pro.local>
2025-09-16 10:22:51 +0900

    initial commit

---
+++ src/main/java/com/slash/project/myfoli/MyFoliTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.slash.project.myfoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFoliTestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFoliTestServerApplication.class, args);
+    }
+
+}
