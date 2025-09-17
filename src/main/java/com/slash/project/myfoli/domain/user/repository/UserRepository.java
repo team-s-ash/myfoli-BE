fe5d34cd4a596a7c0311a3c54b3699d1f6e34bc7
김민준 <ming@Minjuns-MacBook-Pro.local>
2025-09-17 19:46:56 +0900

    add : repository 추가 및 수정

--- src/main/java/com/slash/project/myfoli/domain/user/repository/UserRepository.java
+++ src/main/java/com/slash/project/myfoli/domain/user/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.slash.project.myfoli.domain.user.repository;
 import com.slash.project.myfoli.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
