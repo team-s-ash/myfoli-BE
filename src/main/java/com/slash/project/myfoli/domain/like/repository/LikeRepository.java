45173fc754b00e69ec27d121b1e974269009f9e3
chanul0817 <s24006@gsm.hs.kr>
2025-12-23 14:34:51 +0900

    댓글, ai

--- src/main/java/com/slash/project/myfoli/domain/like/repository/LikeRepository.java
+++ src/main/java/com/slash/project/myfoli/domain/like/repository/LikeRepository.java
@@ -1,14 +1,9 @@
 package com.slash.project.myfoli.domain.like.repository;
 
 import com.slash.project.myfoli.domain.like.entity.Like;
-import com.slash.project.myfoli.domain.post.entity.Post;
-import com.slash.project.myfoli.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    boolean existsByPostAndUser(Post post, User user);
-    Optional<Like> findByPostAndUser(Post post, User user);
-    int countByPost(Post post);
 }
